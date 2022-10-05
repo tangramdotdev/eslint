66e39f270abf3e0902e0a1570025bd9448a30268
David Yamnitsky <david@yamnitsky.com>
2022-10-05 11:34:19 -0400

    Add prettier and editorconfig, do not restrict classes and functions.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -45,9 +45,6 @@ module.exports = {
 		"no-restricted-syntax": [
 			"error",
 			"BinaryExpression[operator='in']",
-			"ClassDeclaration",
-			"FunctionDeclaration",
-			"FunctionExpression",
 			"WithStatement",
 		],
 		"object-shorthand": "error",
